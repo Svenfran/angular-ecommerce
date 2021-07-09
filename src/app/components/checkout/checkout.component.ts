5a7bcfd27de6f8fd8996c6b4c29e50c7b21dbfa0
Sven <sven.haider@web.de>
2021-07-09 10:31:31 +0200

    implement fornt- and backend for order-history

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -30,6 +30,8 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
+  storage: Storage = sessionStorage;
+
   constructor(private formBuilder: FormBuilder,
               private luv2ShopFormService: Luv2ShopFormService,
               private cartService: CartService,
@@ -40,6 +42,9 @@ export class CheckoutComponent implements OnInit {
 
     this.reviewCartDetails();
 
+    // read user's email address form session storage
+    const theEmail = JSON.parse(this.storage.getItem('userEmail'));
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('', [Validators.required, 
@@ -48,7 +53,7 @@ export class CheckoutComponent implements OnInit {
         lastName: new FormControl('', [Validators.required, 
                                       Validators.minLength(2),
                                       Luv2ShopValidators.notOnlyWhitespace]),
-        email: new FormControl('', [Validators.required, 
+        email: new FormControl(theEmail, [Validators.required, 
                                     Validators.pattern('^[a-z0-9_.+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
